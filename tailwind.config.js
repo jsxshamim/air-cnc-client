02ddbd48474983423823eb678a2a303f4f1a29cf
wpshamim <wpshamim@gmail.com>
2022-08-17 02:44:04 +0600

    fix some issue on home service slider

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {},
-    container: {
-      padding: {
-        DEFAULT: "1rem",
-      },
-    },
-  },
-  daisyui: {
-    themes: [
-      {
-        airCncTheme: {
-          primary: "#2bde8c",
-          secondary: "#00a799",
-          accent: "#7bf04d",
-          neutral: "#3D4451",
-          "base-100": "#FFFFFF",
-          info: "#3ABFF8",
-          success: "#36D399",
-          warning: "#FBBD23",
-          error: "#F87272",
+    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+    theme: {
+        extend: {},
+        container: {
+            padding: {
+                DEFAULT: "1rem",
+                "2xl": "5rem",
+            },
         },
-      },
-    ],
-  },
-  plugins: [require("daisyui")],
+    },
+    daisyui: {
+        themes: [
+            {
+                airCncTheme: {
+                    primary: "#2bde8c",
+                    secondary: "#133a45",
+                    accent: "#7bf04d",
+                    neutral: "#3D4451",
+                    "base-100": "#FFFFFF",
+                    info: "#3ABFF8",
+                    success: "#36D399",
+                    warning: "#FBBD23",
+                    error: "#F87272",
+                },
+            },
+        ],
+    },
+    plugins: [require("daisyui")],
 };
