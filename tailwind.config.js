fd1d7ea00b345cae5b3cb6df6595a503ba6d2adc
wpshamim <wpshamim@gmail.com>
2022-08-18 22:59:57 +0600

    fix conflict issues

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-
     content: ["./src/**/*.{js,jsx,ts,tsx}"],
     theme: {
         extend: {},
@@ -10,7 +9,7 @@ module.exports = {
             {
                 airCncTheme: {
                     primary: "#2bde8c",
-                    secondary: "#133a45",
+                    secondary: "#00a799",
                     accent: "#7bf04d",
                     neutral: "#3D4451",
                     "base-100": "#FFFFFF",
@@ -22,23 +21,5 @@ module.exports = {
             },
         ],
     },
-  },
-  daisyui: {
-    themes: [
-      {
-        airCncTheme: {
-          primary: "#2bde8c",
-          secondary: "#00a799",
-          accent: "#7bf04d",
-          neutral: "#3D4451",
-          "base-100": "#FFFFFF",
-          info: "#3ABFF8",
-          success: "#36D399",
-          warning: "#FBBD23",
-          error: "#F87272",
-        },
-      },
-    ],
-  },
-  plugins: [require("daisyui")],
+    plugins: [require("daisyui")],
 };
